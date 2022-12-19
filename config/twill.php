a59a5faab080cc4471fd219d03a586fa5e6631bf
brusnitsyn <andrew.brusnitsyn@gmail.com>
2022-12-19 18:43:30 +0900

    [chg]: media endpoint

--- config/twill.php
+++ config/twill.php
@@ -25,7 +25,7 @@ return [
     ],
     'media_library' => [
         'disk' => 'public',
-        'endpoint_type' => env('MEDIA_LIBRARY_ENDPOINT_TYPE', 's3'),
+        'endpoint_type' => env('MEDIA_LIBRARY_ENDPOINT_TYPE', 'local'),
         'cascade_delete' => env('MEDIA_LIBRARY_CASCADE_DELETE', false),
         'local_path' => env('MEDIA_LIBRARY_LOCAL_PATH', 'uploads/frontend/media'),
         'image_service' => env('MEDIA_LIBRARY_IMAGE_SERVICE', 'A17\Twill\Services\MediaLibrary\Local'),
